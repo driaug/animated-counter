4243a01f8048a06b7ad0c818714ad03d12f3a0b8
Dries Augustyns <inbox@driaug.com>
2023-02-12 12:52:51 +0100

    Initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles/index.css";
+import Counter from "./components/Counter";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section>
+        <h2>
+          <Counter value={100} />
+        </h2>
+      </section>
+      <section>
+        <h2>
+          <Counter value={1000} />
+        </h2>
+      </section>
+    </>
   );
 }
-
-export default App;
---
+++ src/components/Counter.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useRef } from "react";
+import { useInView, useMotionValue, useSpring } from "framer-motion";
+
+/**
+ *
+ * @param root0
+ * @param root0.value
+ */
+export default function Counter({ value }: { value: number }) {
+  const ref = useRef<HTMLSpanElement>(null);
+  const motionValue = useMotionValue(0);
+  const springValue = useSpring(motionValue);
+  const isInView = useInView(ref, { once: true, margin: "-250px" });
+
+  useEffect(() => {
+    if (isInView) {
+      motionValue.set(value);
+    }
+  }, [motionValue, value, isInView]);
+
+  useEffect(
+    () =>
+      springValue.on("change", (latest) => {
+        if (ref.current) {
+          ref.current.textContent = Intl.NumberFormat("en-US").format(
+            latest.toFixed(0)
+          );
+        }
+      }),
+    [springValue]
+  );
+
+  return <span ref={ref} />;
+}
