2c7ee1efc73ec7e5a8ec84adb202fd8aae62c315
Jefferson Lopez <118952535+jefferson-lopez-dev@users.noreply.github.com>
2024-04-14 01:47:48 +0000

    set a default value

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -6,26 +6,26 @@ import { useInView, useMotionValue, useSpring } from "framer-motion";
  * @param root0
  * @param root0.value
  */
-export default function Counter({
-  value,
-  direction = "up",
-}: {
+
+interface Props {
   value: number;
   direction?: "up" | "down";
-}) {
+}
+
+export function Counter({ value, direction = "up" }: Props) {
   const ref = useRef<HTMLSpanElement>(null);
   const motionValue = useMotionValue(direction === "down" ? value : 0);
   const springValue = useSpring(motionValue, {
     damping: 100,
     stiffness: 100,
   });
-  const isInView = useInView(ref, { once: true, margin: "-100px" });
+  const isInView = useInView(ref, { once: true, margin: "-10px" });
 
   useEffect(() => {
     if (isInView) {
       motionValue.set(direction === "down" ? 0 : value);
     }
-  }, [motionValue, isInView]);
+  }, [motionValue, isInView, value, direction]);
 
   useEffect(
     () =>
@@ -36,8 +36,8 @@ export default function Counter({
           );
         }
       }),
-    [springValue]
+    [springValue, value, ref]
   );
 
-  return <span ref={ref} />;
+  return <span ref={ref}>0</span>;
 }
